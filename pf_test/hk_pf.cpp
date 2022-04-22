a1f7fa57758d87bfc566836c5ac66ce81201a53f
极诣·剑魂 <84119280+helloobaby@users.noreply.github.com>
2022-04-22 17:28:37 +0800

    add all srcs

---
+++ pf_test/hk_pf.cpp
@@ -0,0 +1,4 @@
+//hooked #pf handler
+
+
+
---
+++ pf_test/x64.asm
@@ -0,0 +1,54 @@
+extern ori_pf : qword
+extern hk_pf_handler : proc
+
+.code
+
+macro_store macro
+	push rax
+	push rcx
+	push rdx
+	push r8
+	push r9
+	push r10
+	push r11
+	push r12
+	push r13
+	push r14
+	push r15
+	push rdi
+	push rsi 
+	push rbx
+	push rbp
+endm
+
+macro_load macro
+	pop rbp
+	pop rbx
+	pop rsi
+	pop rdi
+	pop r15
+	pop r14
+	pop r13
+	pop r12
+	pop r11
+	pop r10
+	pop r9
+	pop r8
+	pop rdx
+	pop rcx
+	pop rax
+endm	
+
+hk_pf proc
+	macro_store
+	sub rsp,20h
+	call hk_pf_handler
+	add rsp,20h
+	macro_load
+	jmp [ori_pf]
+hk_pf endp
+
+
+
+
+end
